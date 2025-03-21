202a5918ad142bb9bc09080f43dc3450b9c7e473
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-03-21 16:40:42 -0600

    Using Redux Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import {useEffect, useState} from "react"
-import {connect} from "react-redux"
-import {setPokemons as setPokemonsActions} from "./actions/index.js";
+import {useEffect} from "react"
 import {getPokemons} from "./api/index.js"
-
+import {useDispatch, useSelector} from "react-redux";
+import {setPokemons} from "./actions/index.js"
 import {Searcher} from "./components/search.jsx"
 import {Col} from "antd/lib/grid/index.js"
 import {PokemonList} from "./components/pokemon-list.jsx"
 import {SVGComponent} from "./static/logo-pokemon.jsx"
 
-function App({pokemons, setPokemon}) {
+function App() {
+    const pokemons = useSelector(state => state.pokemons)
+    const dispatch = useDispatch()
+
     useEffect(() => {
         const fetchPokemons = async () => {
             try {
                 const {data} = await getPokemons()
-                setPokemon(data.results)
+                dispatch(setPokemons(data.results))
             } catch (error) {
                 console.log(error)
             }
@@ -37,11 +39,5 @@ function App({pokemons, setPokemon}) {
     )
 }
 
-const mapStateToProps = (state) => ({
-    pokemons: state.pokemons,
-})
-const mapDispatchToProps = (dispatch) => ({
-    setPokemon: (payload) => dispatch(setPokemonsActions(payload)),
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
