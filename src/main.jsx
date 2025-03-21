67c92ba8e7001fa9da04ef61d0d0b91a4e53fc1d
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-03-21 16:40:42 -0600

    Using Redux Hooks
    - redux in browser and  Webstorm

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import {createRoot} from 'react-dom/client'
 import {pokemonsReducer} from "./reducers/pokemons.js"
 import {Provider} from 'react-redux'
 import {legacy_createStore as createStore} from 'redux';
+// import { devToolsEnhancer } from "@redux-devtools/remote";
 import App from './App.jsx'
 import './index.css'
 
 
-const store = createStore(pokemonsReducer)
+// const store = createStore(pokemonsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() && devToolsEnhancer({ realtime: true, hostname: "localhost", port: 8001 }))
+const store = createStore(pokemonsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
